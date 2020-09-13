6eb3376c7dc442c5429908c4196f451f70ca1e88
a1930yn <a1930yn@aiit.ac.jp>
2020-09-13 20:28:20 +0900

    1st commit

---
+++ src/main/java/com/example/skillcatchupsep/SkillcatchupsepApplication.java
@@ -0,0 +1,13 @@
+package com.example.skillcatchupsep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillcatchupsepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillcatchupsepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skillcatchupsep/SkillcatchupsepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skillcatchupsep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillcatchupsepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
