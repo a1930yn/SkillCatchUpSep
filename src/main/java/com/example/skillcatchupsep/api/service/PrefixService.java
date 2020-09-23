3535676f66be1985250dfb13b6074c2309dd9af3
a1930yn <a1930yn@aiit.ac.jp>
2020-09-23 22:57:49 +0900

    PrefixService created

---
+++ src/main/java/com/example/skillcatchupsep/api/service/PrefixService.java
@@ -0,0 +1,27 @@
+package com.example.skillcatchupsep.api.service;
+
+import com.example.skillcatchupsep.api.repository.PrefixRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PrefixService {
+
+    @Autowired
+    PrefixRepository prefixRepository;
+
+
+    public List<PrefixEntity> findAll(){
+        return prefixRepository.findAll();
+    }
+
+    public List<PrefixEntity> findTop30(){
+        return prefixRepository.findTop30();
+    }
+
+    public void create(PrefixEntity prefixEntity){
+        prefixRepository.create(prefixEntity);
+    }
+}
